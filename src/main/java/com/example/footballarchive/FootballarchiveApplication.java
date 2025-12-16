0793060b75cb77799ac395da8bd180e69f08eb5b
Pranav Thakur <thakur.pranav109@gmail.com>
2025-12-16 16:57:16 +0530

    initial commit

---
+++ src/main/java/com/example/footballarchive/FootballarchiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.footballarchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballarchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballarchiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/footballarchive/FootballarchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.footballarchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballarchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
