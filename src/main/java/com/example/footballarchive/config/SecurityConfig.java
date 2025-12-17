73ff9cdf2ae0e9dabd8a7e71fba214c42231ad1d
Pranav Thakur <thakur.pranav109@gmail.com>
2025-12-17 13:45:03 +0530

    initial commit

---
+++ src/main/java/com/example/footballarchive/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.example.footballarchive.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/knowledge/public").permitAll()
+                        .requestMatchers("/api/knowledge").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/api/knowledge/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                ).httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+
+        UserDetails user = User.withUsername("user")
+                .password("{noop}user123")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}admin123")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+}
